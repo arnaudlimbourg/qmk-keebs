58ab69d8ad9aaec9a3a739b82e5faf9eac1d9a59
Arnaud Limbourg <arnaud@limbourg.com>
2019-07-21 20:18:03 +0200

    Add my layout

---
+++ ergodox_ez/keymap.c
@@ -0,0 +1,141 @@
+// Arnaud's French AZERTY version of the default_osx file
+#include QMK_KEYBOARD_H
+// #include "ergodox_ez.h"
+#include "version.h"
+#include "keymap_french_osx.h"
+
+#define BASE 0 // default layer
+#define SYMB 1 // symbols
+#define MDIA 2 // media keys
+
+enum custom_keycodes {
+  PLACEHOLDER = SAFE_RANGE, // can always be here
+  EPRM,
+  RGB_SLD,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Keymap 0: Basic layer
+*/
+// If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// Otherwise, it needs KC_*
+[BASE] = LAYOUT_ergodox(  // layer 0 : default
+        // left hand
+        FR_MINS,          FR_AMP,      FR_EACU,       FR_QUOT, FR_APOS, FR_LPRN, LCTL(LSFT(KC_TAB)),
+        KC_TAB,           FR_A,        FR_Z,          KC_E,    KC_R,    KC_T,    MEH_T(KC_DELT),
+        LSFT(KC_LGUI),    FR_Q,        KC_S,          KC_D,    KC_F,    KC_G,
+        KC_LSFT,          CTL_T(FR_W), KC_X,          KC_C,    KC_V,    KC_B,    GUI_T(KC_ESCAPE),
+        LT(SYMB,FR_UGRV), FR_LESS,     LSFT(KC_LALT), KC_LEFT, KC_RGHT,
+
+            // thumb cluster
+            KC_LALT, KC_LCTRL,
+            KC_HOME,
+            KC_SPC, KC_BSPC, KC_END,
+        // right hand
+        LCTL(KC_TAB),     FR_SECT, FR_EGRV,     FR_EXLM,           FR_CCED, FR_AGRV,          FR_RPRN,
+        KC_HYPR,          KC_Y,    KC_U,        KC_I,              KC_O,    KC_P,             KC_BSLASH,
+                          KC_H,    KC_J,        KC_K,              KC_L,    FR_M,             RSFT(KC_RGUI),
+        GUI_T(KC_ESCAPE), KC_N,    FR_COMM,     LT(MDIA, FR_SCLN), FR_COLN, RCTL_T(FR_EQL),   KC_RSFT,
+        KC_UP,            KC_DOWN, KC_LBRC,     KC_RBRC,           TT(SYMB),
+
+            // thumb cluster
+            LGUI(KC_SPACE),  KC_RALT,
+            KC_PGUP,
+            KC_PGDN, KC_TAB, KC_ENT
+    ),
+
+/* Keymap 1: Symbol Layer */
+// SYMBOLS
+[SYMB] = LAYOUT_ergodox(
+      // left hand
+    KC_TRNS,   KC_F1,    KC_F2,           KC_F3,          KC_F4,          KC_F5,    KC_TRNS,
+    KC_TRNS,   FR_BSLS,  KC_KP_ASTERISK,  FR_DLR,         FR_LCBR,        FR_RCBR,  TT(MDIA),
+    KC_TRNS,   FR_EQL,   FR_PIPE,         KC_KP_SLASH,    FR_LPRN,        FR_RPRN,
+    KC_TRNS,   FR_TILD,  FR_AT,           FR_HASH,        FR_LBRC,        FR_RBRC,  KC_TRNS,
+    KC_TRNS,   KC_TRNS,  KC_TRNS,         KC_TRNS,        KC_TRNS,
+
+    // thumb cluster
+    RGB_MOD,  RGB_SLD,
+    KC_TRNS,  RGB_HUD, RGB_HUI, KC_TRNS,
+
+    // right hand
+    KC_TRNS,  KC_F6,          KC_F7,     KC_F8,   KC_F9,     KC_F10,      KC_F11,
+    KC_TRNS,  KC_KP_SLASH,    FR_7,      FR_8,    FR_9,      FR_ASTR,     KC_F12,
+              FR_MINS,        FR_4,      FR_5,    FR_6,      FR_PLUS,     FR_EQL,
+    KC_TRNS,  FR_0,           FR_1,      FR_2,    FR_3,      KC_KP_SLASH, KC_TAB,
+                              KC_TRNS,   KC_TRNS, KC_KP_DOT, KC_EQL,      TO(BASE),
+
+    // thumb cluster
+    RGB_TOG,RGB_SLD,RGB_MOD,RGB_SLD,RGB_VAD,RGB_VAI
+),
+
+/* Keymap 2: Media and mouse keys
+ */
+// MEDIA AND MOUSE
+LAYOUT_ergodox(
+       TO(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
+                                           KC_TRNS, KC_TRNS,
+                                                    KC_TRNS,
+                                  KC_TRNS, KC_TRNS, KC_TRNS,
+    // right hand
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
+                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_WBAK
+),
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  // MACRODOWN only works in this function
+      switch(id) {
+        case 0:
+        if (record->event.pressed) {
+          register_code(KC_RSFT);
+        } else {
+          unregister_code(KC_RSFT);
+        }
+        break;
+      }
+    return MACRO_NONE;
+};
+
+// Runs just one time when the keyboard initializes.
+void matrix_init_user(void) {
+
+};
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) {
+
+    uint8_t layer = biton32(layer_state);
+
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+    switch (layer) {
+      // TODO: Make this relevant to the ErgoDox EZ.
+        case 1:
+            ergodox_right_led_1_on();
+            break;
+        case 2:
+            ergodox_right_led_2_on();
+            break;
+        case 3:
+            ergodox_right_led_3_on();
+            break;
+        default:
+            // none
+            break;
+    }
+
+};
--- ergodox_ez/rules.mk
+++ ergodox_ez/rules.mk
@@ -1,4 +1,21 @@
 # https://github.com/qmk/qmk_firmware/blob/master/docs/getting_started_make_guide.md
-ifndef QUANTUM_DIR
-    include ../../../../Makefile
-endif
+#
+# https://www.reddit.com/r/olkb/comments/bmpgjm/programming_help/
+# Should shave 2000 bytes
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+
+# Enable debugging only when needed.
+CONSOLE_ENABLE = yes # +400 bytes (hid_listen support)
+
+# Enable combo keys for vim usage.
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_combo.md
+# COMBO_ENABLE = yes
+
+# This allows the keyboard to tell the host OS that up to 248 keys are held down at once
+# NKRO_ENABLE = yes # note: also needs FORCE_NKRO in config.h
+
+# Disable unused features to save on space
+# https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+MOUSEKEY_ENABLE = no # 2000 bytes
+BOOTMAGIC_ENABLE = no
+COMMAND_ENABLE = no # https://beta.docs.qmk.fm/features/feature_command
