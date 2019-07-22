3df27b9f0afa337cd91b26da906ec71a4f871423
Arnaud Limbourg <arnaud@limbourg.com>
2019-07-22 08:11:58 +0200

    Add led lighting
    Copy pasted from ivan's

--- ergodox_ez/keymap.c
+++ ergodox_ez/keymap.c
@@ -124,3 +124,51 @@ void matrix_scan_user(void) {
     }
 
 };
+
+// only runs when when the layer is changed, good for updating LED's and clearing sticky state
+uint32_t layer_state_set_user(uint32_t state) {
+  uint8_t layer = biton32(state);
+
+  ergodox_board_led_off();
+  ergodox_right_led_1_off();
+  ergodox_right_led_2_off();
+  ergodox_right_led_3_off();
+
+  combo_enable(); // by default, enable combos.
+  switch (layer) {
+    case 0:
+      break;
+    case 1:
+      clear_mods();
+      ergodox_right_led_1_on();
+      break;
+    case 2:
+      clear_mods();
+      ergodox_right_led_2_on();
+      break;
+    case 3:
+      clear_mods();
+      ergodox_right_led_3_on();
+      break;
+    case 4:
+      ergodox_right_led_1_on();
+      ergodox_right_led_2_on();
+      break;
+    case 5:
+      ergodox_right_led_1_on();
+      ergodox_right_led_3_on();
+      break;
+    case 6:
+      ergodox_right_led_2_on();
+      ergodox_right_led_3_on();
+      break;
+    case 7:
+      ergodox_right_led_1_on();
+      ergodox_right_led_2_on();
+      ergodox_right_led_3_on();
+      break;
+    default:
+      break;
+  }
+  return state;
+};
